3efab7e0c33698908819f537d83b70fdf00a5066
Medicine Yeh <medicinehy@gmail.com>
2017-01-13 20:48:18 +0800

    Fix header problems on older compilers, especially PRIu64
    Referenced from QEMU qemu/osdep.h, I added up several macros
    and headers. It should work on every platform.
    
    The error message might look like:
    qemu_vpmu/vpmu/simulator/Cortex-A9.cc:2910:31: note: in
    expansion of macro ‘CONSOLE_LOG’
     #define CONSOLE_U64(str, val) CONSOLE_LOG(str " %'" PRIu64 "\n",
    (uint64_t)val)
                                   ^
    qemu_vpmu/vpmu/simulator/Cortex-A9.cc:2936:9: note: in
    expansion of macro ‘CONSOLE_U64’
             CONSOLE_U64("  ->Supervisor mode load count :",
    insn.data.system.load);
             ^
    qemu_vpmu/vpmu/simulator/Cortex-A9.cc:2910:53: error:
    expected ‘)’ before ‘PRIu64’
     #define CONSOLE_U64(str, val) CONSOLE_LOG(str " %'" PRIu64 "\n",
    (uint64_t)val)
                                                         ^
    qemu_vpmu/vpmu/include/vpmu-log.h:41:47: note: in
    definition of macro ‘CONSOLE_LOG’
     #define CONSOLE_LOG(str, ...) fprintf(stderr, str, ##__VA_ARGS__)

--- vpmu/include/vpmu-common.h
+++ vpmu/include/vpmu-common.h
@@ -1,17 +1,36 @@
 #ifndef __VPMU_COMMON_H_
 #define __VPMU_COMMON_H_
 
+/* Older versions of C++ don't get definitions of various macros from
+ * stdlib.h unless we define these macros before first inclusion of
+ * that system header.
+ */
+#ifndef __STDC_CONSTANT_MACROS
+#define __STDC_CONSTANT_MACROS
+#endif
+#ifndef __STDC_LIMIT_MACROS
+#define __STDC_LIMIT_MACROS
+#endif
+#ifndef __STDC_FORMAT_MACROS
+#define __STDC_FORMAT_MACROS
+#endif
+
 #include <stdlib.h>  // standard library
 #include <stdio.h>   // standard I/O
 #include <stdint.h>  // uint8_t, uint32_t, etc.
 #include <time.h>    // time_spec
 #include <stdbool.h> // bool, true, false
 
+#include <limits.h>   // Limits of integer types: INT_MAX, ULONG_MAX, etc.
 #include <inttypes.h> // Portable number types, ex: PRIu64, etc.
 #include <locale.h>   // C localization functions for ' support in printf
 #include <string.h>   // For some basic string operations
 #include <stdarg.h>   // va_list, va_start, va_arg, va_end
 #include <sys/stat.h> // open(const char *path, int oflag, ... );
 #include <fcntl.h>    // O_RDONLY, O_WRONLY, O_RDWR
+
+#ifdef __OpenBSD__
+#include <sys/signal.h>
+#endif
 
 #endif
