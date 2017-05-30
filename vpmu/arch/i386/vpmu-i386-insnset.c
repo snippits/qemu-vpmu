1f2a5b70f22e782f587cfc41a63bcb801936e2af
Medicine Yeh <medicinehy@gmail.com>
2017-05-30 12:26:16 +0800

    Remove dead code

--- vpmu/arch/i386/vpmu-i386-insnset.c
+++ vpmu/arch/i386/vpmu-i386-insnset.c
@@ -18,22 +18,3 @@ X86_Instructions get_index_of_x86_insn(const char *s)
 #undef etype
     return X86_INSTRUCTION_TOTAL_COUNTS;
 }
-
-#ifdef CONFIG_VPMU_VFP
-// Return array length if not found
-X86_VFP_Instructions get_index_of_arm_vfp_insn(const char *s)
-{
-#define etype(x) macro_str(x)
-    // static is for putting it in global space
-    static const char *str_x86_vfp_instructions[] = {X86_VFP_INSTRUCTION};
-    int                i;
-
-    for (i = 0; i < sizeof(str_x86_vfp_instructions) / sizeof(const char *); i++) {
-        if (strcmp(str_x86_vfp_instructions[i], s) == 0) return (X86_VFP_Instructions)i;
-    }
-
-    ERR_MSG("get_index_of_x86_vfp_insn: could not find field \"%s\"\n", s);
-#undef etype
-    return X86_VFP_INSTRUCTION_TOTAL_COUNTS;
-}
-#endif
