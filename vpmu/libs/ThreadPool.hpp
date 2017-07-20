f0aff4d9e849bcf33714524b52383c9f6d2e30ae
Medicine Yeh <medicinehy@gmail.com>
2017-07-20 22:34:55 +0800

    [Refactor] ThreadPool and make compiler errors easy to read

--- vpmu/libs/ThreadPool.hpp
+++ vpmu/libs/ThreadPool.hpp
@@ -1,98 +1,120 @@
-#ifndef THREAD_POOL_H
-#define THREAD_POOL_H
+#ifndef _THREAD_POOL_HPP_
+#define _THREAD_POOL_HPP_
+#pragma once
 
 #include <vector>
 #include <queue>
 #include <memory>
 #include <thread>
 #include <mutex>
+#include <atomic>
 #include <condition_variable>
 #include <future>
 #include <functional>
 #include <stdexcept>
+#include <type_traits>
 
-class ThreadPool {
-public:
-    ThreadPool(size_t);
-    template<class F, class... Args>
-    auto enqueue(F&& f, Args&&... args) 
-        -> std::future<typename std::result_of<F(Args...)>::type>;
-    ~ThreadPool();
-private:
-    // need to keep track of threads so we can join them
-    std::vector< std::thread > workers;
-    // the task queue
-    std::queue< std::function<void()> > tasks;
-    
-    // synchronization
-    std::mutex queue_mutex;
-    std::condition_variable condition;
-    bool stop;
-};
- 
-// the constructor just launches some amount of workers
-inline ThreadPool::ThreadPool(size_t threads)
-    :   stop(false)
+class ThreadPool
 {
-    for(size_t i = 0;i<threads;++i)
-        workers.emplace_back(
-            [this]
-            {
-                for(;;)
-                {
+public:
+    ThreadPool(const ThreadPool& rhs) = delete;            // Non-copyable.
+    ThreadPool& operator=(const ThreadPool& rhs) = delete; // Non-assignable.
+
+    // the constructor just launches some amount of workers
+    ThreadPool(size_t threads) : stop(false)
+    {
+        for (size_t i = 0; i < threads; ++i)
+            workers.emplace_back([this] {
+                for (;;) {
                     std::function<void()> task;
 
                     {
+                        // This is now thread independent lock
                         std::unique_lock<std::mutex> lock(this->queue_mutex);
-                        this->condition.wait(lock,
-                            [this]{ return this->stop || !this->tasks.empty(); });
-                        if(this->stop && this->tasks.empty())
-                            return;
+                        this->condition.wait(
+                          lock, [this] { return this->stop || !this->tasks.empty(); });
+                        if (this->stop && this->tasks.empty()) return;
                         task = std::move(this->tasks.front());
                         this->tasks.pop();
                     }
 
                     task();
                 }
-            }
-        );
-}
+            });
+    }
 
-// add new work item to the pool
-template<class F, class... Args>
-auto ThreadPool::enqueue(F&& f, Args&&... args) 
-    -> std::future<typename std::result_of<F(Args...)>::type>
-{
-    using return_type = typename std::result_of<F(Args...)>::type;
+    ~ThreadPool()
+    {
+        {
+            std::unique_lock<std::mutex> lock(queue_mutex);
+            stop = true;
+        }
+        condition.notify_all();
+        for (std::thread& worker : workers) worker.join();
+    }
 
-    auto task = std::make_shared< std::packaged_task<return_type()> >(
-            std::bind(std::forward<F>(f), std::forward<Args>(args)...)
-        );
-        
-    std::future<return_type> res = task->get_future();
+    // add new work item to the pool
+    template <class F, class... Args>
+    auto enqueue(F&& f, Args&&... args)
+    {
+        auto boundTask   = std::bind(std::forward<F>(f), std::forward<Args>(args)...);
+        using ResultType = std::result_of_t<decltype(boundTask)()>;
+
+        // These two are for un-used local typedef
+        ResultType t;
+        (void)t;
+
+        // Help to suppress compiler errors when args does not match args in lambda
+        return enqueue_helper(boundTask, f, std::forward<Args>(args)...);
+    }
+
+    void enqueue_static(std::function<void(void*)> task, void* ptr)
     {
         std::unique_lock<std::mutex> lock(queue_mutex);
-
         // don't allow enqueueing after stopping the pool
-        if(stop)
-            throw std::runtime_error("enqueue on stopped ThreadPool");
-
-        tasks.emplace([task](){ (*task)(); });
+        if (stop) throw std::runtime_error("enqueue on stopped ThreadPool");
+        tasks.emplace([task, ptr]() { task(ptr); });
     }
-    condition.notify_one();
-    return res;
-}
 
-// the destructor joins all threads
-inline ThreadPool::~ThreadPool()
-{
+    void enqueue_static(std::function<void(void*, void*)> task, void* env, void* ptr)
     {
         std::unique_lock<std::mutex> lock(queue_mutex);
-        stop = true;
+        // don't allow enqueueing after stopping the pool
+        if (stop) throw std::runtime_error("enqueue on stopped ThreadPool");
+        tasks.emplace([task, env, ptr]() { task(env, ptr); });
     }
-    condition.notify_all();
-    for(std::thread &worker: workers)
-        worker.join();
-}
+
+private:
+    // Help to suppress compiler errors when args does not match args in lambda
+    template <class B, class F, class... Args>
+    inline auto enqueue_helper(B&& boundTask, F&& f, Args&&... args)
+    {
+        using ResultType = std::result_of_t<decltype(boundTask)()>;
+        auto task        = std::make_shared<std::packaged_task<ResultType()>>(boundTask);
+
+        std::future<ResultType> res = task->get_future();
+        {
+            std::unique_lock<std::mutex> lock(queue_mutex);
+
+            // don't allow enqueueing after stopping the pool
+            if (stop) throw std::runtime_error("enqueue on stopped ThreadPool");
+
+            tasks.emplace([task]() { (*task)(); });
+        }
+        condition.notify_one();
+        return res;
+    }
+
+private:
+    // need to keep track of threads so we can join them
+    std::vector<std::thread> workers;
+    // the task queue
+    std::queue<std::function<void()>> tasks;
+
+    // synchronization
+    std::mutex              queue_mutex;
+    std::condition_variable condition;
+    std::atomic_bool        stop;
+};
 
 #endif
